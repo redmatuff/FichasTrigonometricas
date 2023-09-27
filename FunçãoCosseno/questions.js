53c7ea03fec6704e0351c621e4174311e2953e84
redmatuff <87490251+redmatuff@users.noreply.github.com>
2023-09-27 01:46:30 -0300

    Função Cosseno e Tangente adicionados

---
+++ FunçãoCosseno/questions.js
@@ -0,0 +1,94 @@
+let questions = 
+[
+    {
+        numb: 1,
+        question: "Domínio e Imagem",
+        videoSrc: "Cos_Questao1.mp4",
+        solVideoSrc: "Cos_Solucao1.mp4",         
+        answer: "C. Dom: ℝ, Im: [-1,1]",
+        options: [
+            "A. Dom:(-1,1) , Im: [-&#960,&#960]",
+            "B. Dom: ℝ, Im: ℝ",
+            "C. Dom: ℝ, Im: [-1,1]",
+            "D. Dom: [-1,1], Im: ℝ "
+        ]
+    },
+    {
+        numb: 2,
+        videoSrc: "Cos_Questao2.mp4",
+        solVideoSrc: "Cos_Solucao2.mp4", 
+        question: "Cortes nos eixos",
+        answer: "A. Eixo x: (πk+π/2 ,0), Eixo y:(0,1)",
+        options: [
+            "A. Eixo x: (πk+π/2 ,0), Eixo y:(0,1)",
+            "B. Eixo x: (πk+π/2,πk+π/2) , Eixo y: (-1,1)",
+            "C. Eixo x: (0,πk) , Eixo y:(0,0) ",
+            "D. Eixo x: (0,0), Eixo y:(πk,0) "
+        ]
+    },
+    {
+        numb: 3,
+        question: "Simetria",
+        videoSrc:"Cos_Questao3.mp4",
+        solVideoSrc: "Cos_Solucao3.mp4", 
+        answer: "B. Função Par",
+        options: [
+            "A. Função Ímpar",
+            "B. Função Par",
+            "C. Função par e impar",
+            "D. Nem uma das duas"
+        ]
+    },
+    {
+        numb: 4,
+        solVideoSrc: "Cos_Solucao4.mp4",
+        question: "Periodicidade. A função se repete em um intervalo de:",
+        videoSrc:"Cos_Questao4.mp4",
+        answer: "A. 2π",
+        options: [
+            "A. 2π",
+            "B. 3&#960",
+            "C. &#960/2",
+            "D. 3&#960/2"
+        ]
+    },
+    {
+        numb: 5,
+        solVideoSrc: "Cos_Solucao5.mp4",
+        question: "Os pontos que a função muda de concavidade são da forma:",
+        videoSrc:"Cos_Questao5.mp4",
+        answer: "D. (kπ+π/2 ,0) para k ∈ Z",
+        options: [
+            "A. (kπ/2,0) para n ∈ Z",
+            "B. (0,nπ) para n ∈ N",
+            "C. (nπ,nπ) para n ∈ N",
+            "D. (kπ+π/2 ,0) para k ∈ Z"
+        ]
+    },
+    {
+        numb: 6,
+        solVideoSrc: "Cos_Solucao6.mp4",
+        question: "Reciproca",
+        videoSrc:"Cos_Questao6.mp4",
+        answer: "C. sec(x)",
+        options: [
+            "A. cos(x)",
+            "B. cossec(x)",
+            "C. sec(x)",
+            "D. cotg(x)"
+        ]
+    },
+    {
+        numb: 7,
+        solVideoSrc: "Cos_Solucao7.mp4",
+        question: "Qual a função Inversa",
+        videoSrc:"Cos_Questao7.mp4",
+        answer: "B. arccos(x)",
+        options: [
+            "A. arctg(x)",
+            "B. arccos(x)",
+            "C. cos(x)",
+            "D. arcsen(x)"
+        ]
+    }
+];
